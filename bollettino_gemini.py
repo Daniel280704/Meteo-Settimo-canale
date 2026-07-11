72c88292fea875a5526621afd5294ff25930b681
Daniel280704 <154078145+Daniel280704@users.noreply.github.com>
2026-07-11 07:55:53 +0200

    Update bollettino_gemini.py

--- bollettino_gemini.py
+++ bollettino_gemini.py
@@ -19,7 +19,7 @@ def interpella_gemini(dati_meteo, info_giornaliere):
     api_key = os.getenv("GEMINI_API_KEY")
     genai.configure(api_key=api_key)
     # MODELLO AGGIORNATO (Versione 1.5)
-    model = genai.GenerativeModel('models/gemini-3.5-flash')
+    model = genai.GenerativeModel('models/gemini-3.1-flash-lite')
     
     oggi_str = datetime.now().strftime("%A %d %B")
     domani_str = (datetime.now() + timedelta(days=1)).strftime("%A %d %B")
